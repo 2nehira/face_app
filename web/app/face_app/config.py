7c778216bf6c044661d4664a2ad5b4363585b438
tunehira <kk.oak6@gmail.com>
2020-01-08 21:50:03 +0900

    edit docker_compose

--- web/app/face_app/config.py
+++ web/app/face_app/config.py
@@ -1,6 +1,8 @@
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///face_app.db'
 # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tune:tune@localhost/face_app'
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@db/face_app'
+
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:tunehira@localhost/face_app'
 
 SECRET_KEY = 'secret key'
